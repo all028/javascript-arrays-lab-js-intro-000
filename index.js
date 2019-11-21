eb38365810db96d5382bfbd01f7b750c1623659c
al <albliu@email.com>
2019-11-21 20:14:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  //var copyKittens = [...kittens]
+  kittens.push(name)
+  return kittens
+}
+
+function destructionPrependKitten(name){
+
+
+}
+
+/*
+function destructivelyRemoveKitten(arrayName){
+
+
+}
+
+
+*/
